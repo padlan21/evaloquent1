ec99d1c979ddb62700e71aaa229ecbb4f07d25c6
padlan21 <101447568+padlan21@users.noreply.github.com>
2022-12-20 16:12:50 +0700

    Add files via upload

---
+++ storage/framework/views/5610e6872324865378d344a9b632760f4a2f19a5.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Eloquent Relationships : Relasi One to One</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="card mt-5">
+            <div class="card-body">
+                <h3 class="text-center"><a href="https://santrikoding.com">www.santrikoding.com</a></h3>
+                <h5 class="text-center my-4">Laravel Eloquent Relationship : One To One</h5>
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Nama User</th>
+                            <th>Nomor Telepon</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $__currentLoopData = $users; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $user): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <tr>
+                            <td><?php echo e($user->name); ?></td>
+                            <td><?php echo e($user->phone->phone); ?></td>
+                        </tr>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\eloquent-relationships\resources\views/users.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/ec00ad870b3f8971395014a62d0886b8ed6f507a.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Eloquent Relationships : Relasi One to Many</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <div class="container">
+        <div class="card mt-5">
+            <div class="card-body">
+                <h3 class="text-center"><a href="https://santrikoding.com">www.santrikoding.com</a></h3>
+                <h5 class="text-center my-4">Laravel Eloquent Relationship : One To Many</h5>
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Judul Post</th>
+                            <th>Komentar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $__currentLoopData = $posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <tr>
+                                <td><?php echo e($post->title); ?></td>
+                                <td>
+                                    <?php $__currentLoopData = $post->comments()->get(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $comment): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                        <div class="card shadow-sm mb-2">
+                                            <div class="card-body">
+                                                <i class="fa fa-comments"></i> <?php echo e($comment->comment); ?>
+
+                                            </div>
+                                        </div>
+                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                </td>
+                            </tr>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\eloquent-relationships\resources\views/posts.blade.php ENDPATH**/ ?>
